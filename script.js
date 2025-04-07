0a05f896a163f56929ec7817f45a8c7ebbd1c2b5
prag211 <guptapragati987@gmail.com>
2025-04-07 18:55:53 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// File: script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("darkModeToggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+  });
+});
+
